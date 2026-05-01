155831f309f1ea3f5868f095ef865e2640ef0e9d
Nova <m01@naudet.ch>
2026-05-01 15:58:54 +0200

    feat: added images to the dark mode toggle

--- src/components/DarkModeSlider.vue
+++ src/components/DarkModeSlider.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { onMounted, ref } from 'vue'
 
+import moon from '@/assets/images/image-moon.png'
+import sun from '@/assets/images/image-sun.png'
+
 const isDark = ref(false)
 
 const toggleTheme = () => {
@@ -30,13 +33,19 @@ onMounted(() => {
 <template>
   <div class="flex justify-between items-center cursor-pointer gap-1" @click="toggleTheme">
     <div
-      class="w-14 h-7 flex items-center bg-gray-300 rounded-full p-1 duration-300 ease-in-out border"
+      class="w-14 h-7 flex items-center justify-between bg-gray-300 rounded-full p-1 duration-300 ease-in-out border"
       :class="isDark ? 'border-white bg-neutral-800' : 'bg-neutral-100 border-black'"
     >
       <div
         class="w-5 h-5 rounded-full shadow-md transform duration-300 ease-in-out"
         :class="isDark ? 'bg-neutral-100' : 'translate-x-7 bg-black'"
       ></div>
+      <img
+        class="h-4 w-4 mr-1"
+        :class="{ '-translate-x-6.5': !isDark }"
+        :src="isDark ? moon : sun"
+        alt="Dark Mode Icon"
+      />
     </div>
   </div>
 </template>
