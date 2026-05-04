af6ab073b7ea89b954397f40b4c2282780b49be5
Nova <m01@naudet.ch>
2026-05-04 14:20:03 +0200

    fix(music card): auto ellipsis on the title

--- src/components/MusicCard.vue
+++ src/components/MusicCard.vue
@@ -23,7 +23,7 @@ function toggleAudioState() {
       class="-mt-20 mb-6 w-full rounded-2xl object-cover shadow-lg"
     />
 
-    <p class="mb-3 text-xl font-medium">{{ title }}</p>
+    <p class="mb-3 text-xl w-full font-medium truncate text-center">{{ title }}</p>
 
     <button @click="toggleAudioState" class="mb-3 cursor-pointer">
       <img :src="isPlaying ? pause : resume" alt="Play/Pause" class="w-10" />
