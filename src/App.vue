e6cab4f0aa0917ec0cd90a22693e6ff10dffd99f
Nova <m01@naudet.ch>
2026-05-05 13:11:16 +0200

    refactor(data): move default tracks from App.vue to dedicated data file

--- src/App.vue
+++ src/App.vue
@@ -3,26 +3,6 @@
 import { computed, onMounted, onUnmounted, ref, markRaw } from 'vue'
 import localforage from 'localforage'
 
-// Assets
-import rainAudio from '@/assets/audio/audio-rain.mp3'
-import rainImage from '@/assets/images/musics/image-rain.png'
-import brownImage from '@/assets/images/musics/image-brown.png'
-import brownAudio from '@/assets/audio/audio-brown.mp3'
-import vinylImage from '@/assets/images/musics/image-vinyl.png'
-import vinylAudio from '@/assets/audio/audio-vinyl.mp3'
-import coffeeImage from '@/assets/images/musics/image-coffee.png'
-import coffeeAudio from '@/assets/audio/audio-coffee.mp3'
-import fireImage from '@/assets/images/musics/image-fire.png'
-import fireAudio from '@/assets/audio/audio-fire.mp3'
-import thunderImage from '@/assets/images/musics/image-thunder.png'
-import thunderAudio from '@/assets/audio/audio-thunder.mp3'
-import beachImage from '@/assets/images/musics/image-beach.png'
-import beachAudio from '@/assets/audio/audio-beach.mp3'
-import keyboardImage from '@/assets/images/musics/image-keyboard.png'
-import keyboardAudio from '@/assets/audio/audio-keyboard.mp3'
-import bubbleImage from '@/assets/images/musics/image-bubble.png'
-import bubbleAudio from '@/assets/audio/audio-bubble.mp3'
-
 // Components
 import MobileHeader from './components/MobileHeader.vue'
 import VolumeSlider from './components/VolumeSlider.vue'
@@ -30,83 +10,20 @@ import AppFooter from './components/AppFooter.vue'
 import MusicCard from './components/MusicCard.vue'
 import AddMusicCard from './components/AddMusicCard.vue'
 import AddMusicPopup from './components/AddMusicPopup.vue'
+import { defaultTracksData } from './data/defaultTrack'
 
 const masterVolume = ref(50)
 
-const musics = ref([
-  {
-    title: 'Rain',
-    image: rainImage,
-    audio: markRaw(new Audio(rainAudio)),
+const musics = ref(
+  defaultTracksData.map((track) => ({
+    title: track.title,
+    image: track.image,
+    audio: markRaw(new Audio(track.audioSrc)),
     isPlaying: false,
     volume: 50,
     isCustom: false,
-  },
-  {
-    title: 'Brown Noise',
-    image: brownImage,
-    audio: markRaw(new Audio(brownAudio)),
-    isPlaying: false,
-    volume: 50,
-    isCustom: false,
-  },
-  {
-    title: 'Vinyl Crack',
-    image: vinylImage,
-    audio: markRaw(new Audio(vinylAudio)),
-    isPlaying: false,
-    volume: 50,
-    isCustom: false,
-  },
-  {
-    title: 'Coffee Shop Ambience',
-    image: coffeeImage,
-    audio: markRaw(new Audio(coffeeAudio)),
-    isPlaying: false,
-    volume: 50,
-    isCustom: false,
-  },
-  {
-    title: 'FirePlace Crackle',
-    image: fireImage,
-    audio: markRaw(new Audio(fireAudio)),
-    isPlaying: false,
-    volume: 50,
-    isCustom: false,
-  },
-  {
-    title: 'Thunderstorm',
-    image: thunderImage,
-    audio: markRaw(new Audio(thunderAudio)),
-    isPlaying: false,
-    volume: 50,
-    isCustom: false,
-  },
-  {
-    title: 'Beach Waves',
-    image: beachImage,
-    audio: markRaw(new Audio(beachAudio)),
-    isPlaying: false,
-    volume: 50,
-    isCustom: false,
-  },
-  {
-    title: 'Keyboard Typing',
-    image: keyboardImage,
-    audio: markRaw(new Audio(keyboardAudio)),
-    isPlaying: false,
-    volume: 50,
-    isCustom: false,
-  },
-  {
-    title: 'Bubbles',
-    image: bubbleImage,
-    audio: markRaw(new Audio(bubbleAudio)),
-    isPlaying: false,
-    volume: 50,
-    isCustom: false,
-  },
-])
+  })),
+)
 
 const activeMusicCount = computed(() => musics.value.filter((m) => m.isPlaying).length)
 const isPopupVisible = ref(false)
---
+++ src/data/defaultTrack.js
@@ -0,0 +1,30 @@
+import rainAudio from '@/assets/audio/audio-rain.mp3'
+import rainImage from '@/assets/images/musics/image-rain.png'
+import brownImage from '@/assets/images/musics/image-brown.png'
+import brownAudio from '@/assets/audio/audio-brown.mp3'
+import vinylImage from '@/assets/images/musics/image-vinyl.png'
+import vinylAudio from '@/assets/audio/audio-vinyl.mp3'
+import coffeeImage from '@/assets/images/musics/image-coffee.png'
+import coffeeAudio from '@/assets/audio/audio-coffee.mp3'
+import fireImage from '@/assets/images/musics/image-fire.png'
+import fireAudio from '@/assets/audio/audio-fire.mp3'
+import thunderImage from '@/assets/images/musics/image-thunder.png'
+import thunderAudio from '@/assets/audio/audio-thunder.mp3'
+import beachImage from '@/assets/images/musics/image-beach.png'
+import beachAudio from '@/assets/audio/audio-beach.mp3'
+import keyboardImage from '@/assets/images/musics/image-keyboard.png'
+import keyboardAudio from '@/assets/audio/audio-keyboard.mp3'
+import bubbleImage from '@/assets/images/musics/image-bubble.png'
+import bubbleAudio from '@/assets/audio/audio-bubble.mp3'
+
+export const defaultTracksData = [
+  { title: 'Rain', image: rainImage, audioSrc: rainAudio },
+  { title: 'Brown Noise', image: brownImage, audioSrc: brownAudio },
+  { title: 'Vinyl Crack', image: vinylImage, audioSrc: vinylAudio },
+  { title: 'Coffee Shop Ambience', image: coffeeImage, audioSrc: coffeeAudio },
+  { title: 'FirePlace Crackle', image: fireImage, audioSrc: fireAudio },
+  { title: 'Thunderstorm', image: thunderImage, audioSrc: thunderAudio },
+  { title: 'Beach Waves', image: beachImage, audioSrc: beachAudio },
+  { title: 'Keyboard Typing', image: keyboardImage, audioSrc: keyboardAudio },
+  { title: 'Bubbles', image: bubbleImage, audioSrc: bubbleAudio },
+]
